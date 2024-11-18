e0d56093ccd429c9457473dff89477e5ff4bebac
Ali Şaffak <ali.saffak@asiselektronik.com.tr>
2024-11-18 09:22:12 +0300

    product endpoint added

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,7 +6,7 @@ namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         :ICommand<CreateProductResult>;
-    public record CreateProductResult(Guid id);
+    public record CreateProductResult(Guid Id);
     internal class CreateProductCommandHanler 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
